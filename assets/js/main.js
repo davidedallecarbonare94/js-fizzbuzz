27b5c39af8762e8936cd3565f368f7d70ceb16de
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-04-27 16:10:57 +0200

    solution found/end project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,19 @@
 //Istruzioni:
 //Scrivi un programma che stampi i numeri da 1 a 100,
-var message;
-var fizz = [];
-var buzz = []
-var fizzBuzz = []
 
 for (var i = 1; i < 101; i++) {
-    console.log(i)
-    if ((i / 3 % 1) == 0) {
-        fizz.push(i)
+    if ((i / 5 % 1) != (i / 3 % 1) == 0){
+     console.log("FizzBuzz");
     } else if ((i / 5 % 1) == 0) {
-        buzz.push(i)
-    } else if ((i / 5 % 1) && (i / 3 % 1) == 0){
-        fizzBuzz.push(i)
+     console.log("Buzz");
+    } else if ((i / 3 % 1) == 0) {
+     console.log("Fizz");
+    } else {
+        console.log(i)
     }
 }
-console.log(fizz)
-console.log(buzz)
-console.log(fizzBuzz)
-//ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 
+
+//ma per i multipli di 3 stampi “Fizz” al posto del numero 
+//e per i multipli di 5 stampi Buzz.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
